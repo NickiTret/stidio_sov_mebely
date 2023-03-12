4e6cf1f6305b513073d71968ba0537fc05a31c33
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-12 16:32:04 +0300

    fix

--- resources/views/ssm/environment/header.blade.php
+++ resources/views/ssm/environment/header.blade.php
@@ -15,10 +15,10 @@
           </svg>
         </a>
         <a href="tel:{{ $mainset->tel }}" class="header__number">{{ $mainset->tel }}</a>
-        @if (auth()->check())
+        {{-- @if (auth()->check())
             <a>{{ user()->name }}</a>
             <a href="{{ route('logout') }}" class="header__number">Выйти</a>
-        @else
+        @else --}}
         <a href="{{ route('login') }}" class="header__number">Войти</a>
         @endif
         <button  class="btn btn-main" data-custom-open="modal-contacts">Заказать звонок</button>
