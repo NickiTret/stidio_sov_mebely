35db35a1c19dbaa324b7e281d7371ba28fbed884
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-12 15:06:01 +0300

    fix favicon

--- resources/views/ssm/environment/head.blade.php
+++ resources/views/ssm/environment/head.blade.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
 <html lang="ru" class="page">
-  <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="theme-color" content="#111111" />
-  <meta name="format-detection" content="telephone=no">
-  <title>Студия Современной Мебели | {{ $seting->title }}</title>
-  <meta property="og:description" content="{{ $seting->descriptions }}">
-  <meta name="keywords" content="{{ $seting->keywords }}" />
-  <link
-    rel="preload"
-    href="fonts/MullerRegular.woff2"
-    as="font"
-    type="font/woff2"
-    crossorigin
-  />
-  <link rel="stylesheet" href="{{ asset('css/vendor.css') }}" />
-  <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"
-  />
-  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-  <script defer src="{{ asset('js/main.js') }}"></script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="theme-color" content="#111111" />
+    <meta name="format-detection" content="telephone=no">
+    <link rel="icon" href="{{ $seting->getImage() }}" type="image/svg+xml">
+    <title>Студия Современной Мебели | {{ $seting->title }}</title>
+    <meta property="og:description" content="{{ $seting->descriptions }}">
+    <meta name="keywords" content="{{ $seting->keywords }}" />
+    <link rel="preload" href="fonts/MullerRegular.woff2" as="font" type="font/woff2" crossorigin />
+    <link rel="stylesheet" href="{{ asset('css/vendor.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script defer src="{{ asset('js/main.js') }}"></script>
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(92774377, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/92774377" style="position:absolute; left:-9999px;" alt="" />
+        </div>
+    </noscript>
 </head>
