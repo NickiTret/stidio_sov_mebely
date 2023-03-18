23199abb5799d99e0decd81203e53f738b130127
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-18 19:44:00 +0300

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -315,6 +315,26 @@ gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.from(items, {
   stagger: 1
 });
 
+// какие-либо параметры
+const options = {
+  // root: document.querySelector( '#viewport' ), // я закомментил строку, чтобы использовать значение по умолчанию
+  rootMargin: '0px',
+  threshold: [0, 0.5]
+};
+const observer = new IntersectionObserver(trueCallback, options);
+const target = document.querySelector('#target');
+observer.observe(target); // запускаем "слежку" за элементом(ами) в константе target
+
+// callback-функция (возвратная функция)
+const trueCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    // делаем что-либо для каждого переданного элемента (в нашем случае он один)
+    console.log('сработало');
+    // например можно добавить какой-либо CSS-класс элементу
+    entry.target.classList.add('some-class');
+  });
+};
+
 /***/ }),
 
 /***/ "./src/js/components/fancybox.js":
--- src/js/components/factoids.js
+++ src/js/components/factoids.js
@@ -45,4 +45,27 @@ gsap.from(items, {
 
 
 
+// какие-либо параметры
+const options = {
+	// root: document.querySelector( '#viewport' ), // я закомментил строку, чтобы использовать значение по умолчанию
+	rootMargin: '0px',
+	threshold: [ 0, 0.5 ]
+};
+
+const observer = new IntersectionObserver( trueCallback, options );
+
+const target = document.querySelector( '#target' );
+observer.observe( target ); // запускаем "слежку" за элементом(ами) в константе target
+
+// callback-функция (возвратная функция)
+const trueCallback = function(entries, observer) {
+	entries.forEach((entry) => {
+		// делаем что-либо для каждого переданного элемента (в нашем случае он один)
+		console.log( 'сработало' );
+		// например можно добавить какой-либо CSS-класс элементу
+		entry.target.classList.add( 'some-class' );
+	});
+}
+
+
 
