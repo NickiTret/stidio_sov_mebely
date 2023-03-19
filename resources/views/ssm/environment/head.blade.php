6b7effac44c00c382597abac69db3e4de4b28113
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-19 16:01:26 +0300

    fix

--- resources/views/ssm/environment/head.blade.php
+++ resources/views/ssm/environment/head.blade.php
@@ -18,11 +18,11 @@
     <link rel="preload" href="fonts/Montserrat-Regular.woff" as="font" type="font/woff" crossorigin />
     <link rel="preload" href="fonts/Montserrat-SemiBold.woff" as="font" type="font/woff" crossorigin />
 
-    <link rel="stylesheet" href="{{ asset('css/vendor.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/vendor.css') }}?v=1" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}?v=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-    <script defer src="{{ asset('js/main.js') }}"></script>
+    <script defer src="{{ asset('js/main.js') }}?v=1"></script>
     <script type="text/javascript">
         (function(m, e, t, r, i, k, a) {
             m[i] = m[i] || function() {
