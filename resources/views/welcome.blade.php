28bbdd1caec77d7b72322c90afb4f2868facf3bf
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-06-12 18:19:45 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,21 @@
 
 
 <body class="page__body">
+    <!--LiveInternet counter--><a href="https://www.liveinternet.ru/click" target="_blank"><img id="licnt1D19"
+            width="31" height="31" style="border:0" title="LiveInternet"
+            src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7" alt="" /></a>
+    <script>
+        (function(d, s) {
+            d.getElementById("licnt1D19").src =
+                "https://counter.yadro.ru/hit?t44.6;r" + escape(d.referrer) +
+                ((typeof(s) == "undefined") ? "" : ";s" + s.width + "*" + s.height + "*" +
+                    (s.colorDepth ? s.colorDepth : s.pixelDepth)) + ";u" + escape(d.URL) +
+                ";h" + escape(d.title.substring(0, 150)) + ";" + Math.random()
+        })
+        (document, screen)
+    </script>
+    <!--/LiveInternet-->
+
     <div class="site-container">
         @include('ssm.environment.header', ['mainset' => $mainset])
 
