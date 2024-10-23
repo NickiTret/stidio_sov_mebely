45255a4f418a103f08ecfbf103f79e0b3cf4eb7b
nickdesignweb <tretjakov.nickit@yandex.ru>
2024-10-23 09:18:54 +0300

    add new edits

--- resources/views/ssm/components/design.blade.php
+++ resources/views/ssm/components/design.blade.php
@@ -13,7 +13,34 @@
                     @endforeach
                 </ul>
                 <div class="tabs__content">
+
                     @foreach ($groups as $slider)
+                        @if (count($slider->slides))
+                            <div class="tabs__panel">
+                                <div class="tabs__panel__grid">
+                                    @foreach ($slider->slides as $item_slide)
+
+                                    <div class="tabs__panel__item">
+                                        <a class="tabs__panel__box" href="{{ $item_slide->getImage() }}"
+                                            data-fancybox="{{ $item_slide->group->title }}"
+                                            data-caption="{{ $slider->title }}">
+                                            <div class="tabs__panel__image">
+                                                <img src="{{ $item_slide->getImage() }}" alt="{{ $item_slide->title }}" />
+                                            </div>
+                                            <h2>{{ $item_slide->title }}</h2>
+                                            <p>{{ $item_slide->description }}</p>
+                                        </a>
+
+                                    </div>
+
+                                    @endforeach
+                                </div>
+
+                            </div>
+                        @endif
+                    @endforeach
+
+                    {{-- @foreach ($groups as $slider)
                         @if (count($slider->slides))
                             <div class="tabs__panel">
                                 <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff;"
@@ -26,10 +53,10 @@
                                                     data-caption="{{ $slider->title }}">
                                                     <img src="{{ $item_slide->getImage() }}"
                                                         alt="{{ $item_slide->title }}" />
-                                                        <div class="swiper-slide__description">
-                                                            <h3>{{ $item_slide->title }}</h3>
-                                                            <p>{{ $item_slide->description }}</p>
-                                                        </div>
+                                                    <div class="swiper-slide__description">
+                                                        <h3>{{ $item_slide->title }}</h3>
+                                                        <p>{{ $item_slide->description }}</p>
+                                                    </div>
                                                 </a>
                                             </div>
                                         @endforeach
@@ -39,8 +66,9 @@
                                 </div>
                             </div>
                         @endif
-                    @endforeach
+                    @endforeach --}}
                 </div>
+
             </div>
         </div>
     </section>
