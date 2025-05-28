6c4b3dbb27d1281ff2d9c60384030c9a6bbd5863
nickdesignweb <tretjakov.nickit@yandex.ru>
2025-05-28 19:58:01 +0300

    fix form

--- resources/views/ssm/components/form.blade.php
+++ resources/views/ssm/components/form.blade.php
@@ -1,4 +1,4 @@
-{{-- <div class="modal overlay" id="modal-contacts">
+<div class="modal overlay" id="modal-contacts">
     <div class="modal__wrapper">
         <h2>Напишите нам</h2>
         <button class="btn btn-close" aria-label="Close modal" data-custom-close>
@@ -6,20 +6,20 @@
         <form action="{{ route('message.store') }}" id="form" method="post" class="form">
             @csrf
             <label class="form__label">
-                <input type="text" name="name" id="name" class="input-reset form__input" placeholder="Имя">
+                <input type="text" name="name" id="name" class="input-reset form__input" placeholder="Имя" disabled>
             </label>
             <label class="form__label">
-                <input type="tel" name="tel" id="tel" class="input-reset form__input"
+                <input type="tel" name="tel" id="tel" class="input-reset form__input" disabled
                     placeholder="Телефон">
             </label>
             <label class="form__label">
-                <input type="email" name="mail" id="email" class="input-reset form__input" placeholder="Почта">
-                <input type="hidden" name="secret" id="secret" value="">
+                <input type="email" name="mail" id="email" class="input-reset form__input" placeholder="Почта" disabled>
+                <input type="hidden" name="secret" id="secret" value="" disabled>
             </label>
             <label class="form__label">
-                <textarea type="text" name="content" class="input-reset form__input" placeholder="Сообщение"></textarea>
+                <textarea type="text" name="content" class="input-reset form__input" placeholder="Сообщение" disabled></textarea>
             </label>
             <button class="btn-reset btn btn-main btn-send" type="submit">Отправить</button>
         </form>
     </div>
-</div> --}}
+</div>
